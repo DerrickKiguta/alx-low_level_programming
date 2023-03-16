67840b9c90cc746da4b9256da5d44aecf593d97f
DerrickGithui <derrickgithui@gmail.com>
2023-03-16 01:10:27 -0700

    proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - use of printf
+ *
+ * Return 0 when Success
+ */
+int main(void)
+{
+	printf("%\n","with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
