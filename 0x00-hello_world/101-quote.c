56f303f5c57d7fc7e0518bbf0900e6b64acee52c
DerrickGithui <derrickgithui@gmail.com>
2023-03-16 02:05:46 -0700

    Final attempt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,30 +1,13 @@
+#include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 1 (Error)
+ * main - use of write
+ *Description: Writes Piece of art is useful
+ *  Return: 1 (error)
  */
+
 int main(void)
 {
-    /**
-     * write - write to a file descriptor
-     * @filedes: the file descriptor to write to
-     * @buf: the buffer to write from
-     * @nbytes: the number of bytes to write
-     *
-     * Description: This function writes @nbytes bytes of the buffer @buf
-     * to the file descriptor @filedes. It returns the number of bytes
-     * written, or -1 on error.
-     *
-     * Return: the number of bytes written, or -1 on error
-     */
-    ssize_t nbytes_written = write(2,
-                                    "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-                                    59);
-
-    if (nbytes_written < 0)
-    {
-        return (1);
-    }
-
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }
