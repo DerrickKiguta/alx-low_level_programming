1ba422c79503f26c61f27d2745924aac96394839
DerrickKiguta <derrickgithui@gmail.com>
2023-03-21 20:39:23 +0300

    which is slower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _islower - checks if lower
+ *@a: char entered
+ * Return: return 1 if lower or 0
+ */
+int _islower(int a)
+{
+	if (a >= 'a' && a < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
