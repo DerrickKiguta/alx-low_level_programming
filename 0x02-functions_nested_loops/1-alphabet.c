c6ed70543a2733c4cdc7a49205cd5928fdd75078
DerrickKiguta <derrickgithui@gmail.com>
2023-03-21 16:22:27 +0300

    The alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints all lower case alphabets
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
