34360752a47b27d265593bc2aeedd02f6b0bf8ab
DerrickKiguta <derrickgithui@gmail.com>
2023-03-21 21:05:05 +0300

    The absolute value

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = -10;
+    int b = 5;
+    printf("The absolute value of %d is %d\n", a, _abs(a));
+    printf("The absolute value of %d is %d\n", b, _abs(b));
+    return (0);
+}
