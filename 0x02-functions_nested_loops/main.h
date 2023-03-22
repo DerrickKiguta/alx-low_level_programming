1fb810142d14db674c52dd7ed6497b5d1d626c34
DerrickKiguta <derrickgithui@gmail.com>
2023-03-22 11:03:37 +0300

    corrections

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN-H
+
 void print_times_table(int n);
 int _putchar(char c);
 void print_alphabet(void);
@@ -11,3 +14,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+#endif
