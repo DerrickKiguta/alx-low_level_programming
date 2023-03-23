c142f7a60c63eed2536748c898950757d7e90b5c
DerrickKiguta <derrickgithui@gmail.com>
2023-03-23 16:26:39 +0300

    prime factors 1

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * largest_prime_factor - finds and returns the largest prime factor of a number
+ * largest_prime_factor - finds the largest prime factor of a given number
  * @n: the number to find the largest prime factor of
  *
  * Return: the largest prime factor of n
@@ -12,9 +12,26 @@ long largest_prime_factor(long n)
 
     for (i = 2; i <= n; i++)
     {
-        while (n % i == 0 && n != i)
+        if (n % i == 0)
+        {
             n /= i;
+            i--;
+        }
     }
 
-    return n;
+    return i;
+}
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+    long n = 612852475143;
+
+    printf("%ld\n", largest_prime_factor(n));
+
+    return 0;
 }
