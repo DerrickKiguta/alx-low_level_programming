207797fd629a565c0473737dd59cd7e580a50539
DerrickKiguta <derrickgithui@gmail.com>
2023-03-27 20:59:25 +0300

    winning is only half

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,16 @@
+
+/**
+ * main - check the code
+ *puts_half declares the string
+ *
+ * Return: Always 0.
+*/
+void puts_half(char *str);
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts_half(str);
+    return (0);
+}
